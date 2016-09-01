f7031730199c6e3a7af81a21c04103bc946e0d70
jonas_aguirre <jonas_aguirre@hotmail.com>
2016-09-01 14:32:19 -0500

    commit new species

---
+++ params/we_aguirre.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <- 4
+
+# e.g. "anas_krystallinus"    
+species.name <- we_aguirre
+
+# e.g. "red"    
+color <- "darkgrey""
+
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
